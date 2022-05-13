0ae67b2ea5b511d435583599e4c883798067187f
kida-works <kida.works.1234@gmail.com>
2022-05-14 00:02:32 +0900

    add

--- app/app.js
+++ app/app.js
@@ -1,10 +1,47 @@
-const { response } = require("express");
+// const { response } = require("express");
 const express = require("express");
 const app = express();
+const sqlite3 = require('sqlite3')
+const dbpath = "app/db/database.sqlite3"
 
+// Get all users(usersのデータを全て取得)
+app.get("/api/v1/users", (req, res) => {
+  // Connect database(データベースに接続)
+  const db = new sqlite3.Database(dbpath)
 
-app.get("/api/v1/hello", (req, res) => {
-  res.json({"message": "Hello, World!"})
+  // usersデータ全て取得
+  db.all('SELECT * FROM users', (err, rows)=>{
+    res.json({"message": "Hello World!"})
+  })
+  db.close()
+});
+
+// Get a user(user個人のデータ全て取得)
+// :id は変数的に扱うことができる
+app.get("/api/v1/users/:id", (req, res) => {
+  // Connect database(データベースに接続)
+  const db = new sqlite3.Database(dbpath)
+  const id = req.params.id
+
+  // userデータ全て取得
+  db.get(`SELECT * FROM users WHERE id = {id}`, (err, row)=>{
+    res.json(row)
+  })
+
+  //Search users matching keyword (検索)
+  app.get("/api/v1/search", (req, res) => {
+    // Connect database(データベースに接続)
+    const db = new sqlite3.Database(dbpath)
+    // データ取得
+    const ketword = req.query.p
+  
+    // userデータ全て取得
+    db.all(`SELECT * FROM users WHERE name = name LIKE "%${keywprd}%"`, (err, row)=>{
+      res.json(row)
+    })
+  })
+  db.close()
+
 });
 
 // ↓ローカルの環境変数にport番号に指定している人がいたら、その人のポート番号を使って表示しますよという命令。指定がないと3000版を使う
